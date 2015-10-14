02be37f6d3b50bb685667cbbbabbcac6f78d4b7a
Yoshioka Kouta <s15012@std.it-college.ac.jp>
2015-10-14 10:54:55 +0900

    first commit

---
+++ Question2.java
@@ -0,0 +1,15 @@
+public class Question2 {
+	public static void main(String[] args) {
+			for (int i = 1; i <= 100; i++){
+				if(i % 3 == 0){
+					System.out.println("THREE");
+				} else if (i >= 30 && i <= 39){
+					System.out.println("THREE");
+				} else if (i % 10 == 3){
+					System.out.println("THREE");
+				}	else {
+					System.out.println(i);
+				}
+			}
+	}
+}
